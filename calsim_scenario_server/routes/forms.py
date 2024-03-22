98e4777ff4c37354dd4501ea864b86adcee709f0
Zachary Roy <zachary.roy@water.ca.gov>
2024-03-22 09:50:19 -0700

    Add form for scenarios

--- calsim_scenario_server/routes/forms.py
+++ calsim_scenario_server/routes/forms.py
@@ -4,11 +4,78 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from ..logger import logger
-from ..models import DCP, SOD, TUCP, VA, Hydrology, LandUse, SeaLevelRise
+from ..models import DCP, SOD, TUCP, VA, Hydrology, LandUse, Scenario, SeaLevelRise
 from . import get_db
 
 router = APIRouter(prefix="/forms")
 templates = Jinja2Templates(directory="calsim_scenario_server/templates")
+
+
+@router.get("/scenarios", response_class=HTMLResponse)
+async def get_scenario_form(request: Request, db: Session = Depends(get_db)):
+    scenarios = db.query(Scenario).all()
+    # The keys to this dict will be converted to ids in the HTML,
+    # those ids should be valid python vars, because they are what FastAPI
+    # passes to the POST request handler
+    assumption_tables = {
+        "Hydrology": Hydrology,
+        "Sea Level Rise": SeaLevelRise,
+        "Land Use": LandUse,
+        "TUCP": TUCP,
+        "DCP": DCP,
+        "VA": VA,
+        "South of Delta Storage": SOD,
+    }
+
+    existing_assumptions = {
+        k: db.query(tbl).all() for k, tbl in assumption_tables.items()
+    }
+
+    return templates.TemplateResponse(
+        "scenario.html",
+        {
+            "request": request,
+            "scenarios": scenarios,
+            "existing_assumptions": existing_assumptions,
+        },
+    )
+
+
+@router.post("/scenarios")
+async def post_scenario_form(
+    scenario_name: str = Form(...),
+    hydrology: int = Form(...),
+    sea_level_rise: int = Form(...),
+    land_use: int = Form(...),
+    tucp: int = Form(...),
+    dcp: int = Form(...),
+    va: int = Form(...),
+    south_of_delta_storage: int = Form(...),
+    db: Session = Depends(get_db),
+):
+    logger.info(f"adding assumption {scenario_name=}")
+    try:
+        row = Scenario(
+            scenario_name=scenario_name,
+            hydrology_id=hydrology,
+            sea_level_rise_id=sea_level_rise,
+            land_use_id=land_use,
+            tucp_id=tucp,
+            dcp_id=dcp,
+            va_id=va,
+            sod_id=south_of_delta_storage,
+        )
+        # Add the new path to the database session
+        db.add(row)
+        db.commit()
+        db.refresh(row)
+
+        return {"message": "Assumption added", "scenario": row}
+
+    except Exception as e:
+        logger.error(f"{type(e)} encountered: {e}")
+        db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/tucp", response_class=HTMLResponse)
