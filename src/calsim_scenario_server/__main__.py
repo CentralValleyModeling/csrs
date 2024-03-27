dc1ba2ec6c71707a1583760b4ea55d6afeaf1222
Zachary Roy <zachary.roy@water.ca.gov>
2024-03-27 14:55:35 -0700

    Separate FastAPI main script and the python entry point logic

--- src/calsim_scenario_server/__main__.py
+++ src/calsim_scenario_server/__main__.py
@@ -1,50 +1,14 @@
-from fastapi import FastAPI
-from fastapi.templating import Jinja2Templates
+import argparse
+import os
 
-from . import __version__
-from .routes import assumptions, forms, paths, runs, scenarios, timeseries, timesteps
-
-TITLE = "CalSim-Results Server"
-SUMMARY = "A FastAPI app to serve CalSim3 model results and metadata."
-DESCRIPTION = """
-Helps you to interact with the CalSim Scnario Database.
-"""
-CONTACT = {
-    "name": "California DWR, Central Valley Modeling",
-    "url": "https://water.ca.gov/Library/"
-    + "Modeling-and-Analysis/Central-Valley-models-and-tools",
-}
-LISCENSE = {
-    "name": "MIT",
-    "identifier": "MIT",
-}
-
-
-app = FastAPI(
-    title=TITLE,
-    summary=SUMMARY,
-    version=__version__,
-    docs_url="/",
-    description=DESCRIPTION,
-    contact=CONTACT,
-    license_info=LISCENSE,
-)
-
-ENABLE_FORMS = False
-
-app.include_router(timeseries.router)
-app.include_router(runs.router)
-app.include_router(scenarios.router)
-app.include_router(assumptions.router)
-app.include_router(paths.router)
-app.include_router(timesteps.router)
-if ENABLE_FORMS:
-    app.include_router(forms.router)
-
-# TODO move this into a sub-module so the routes can interact with them easily
-templates = Jinja2Templates(directory="./templates")
+parser = argparse.ArgumentParser()
+parser.add_argument("--database-name", default="example")
+args = parser.parse_args()
+os.environ["DATABASE_NAME"] = args.database_name
 
 if __name__ == "__main__":
     import uvicorn
+
+    from .main import app
 
     uvicorn.run(app)
