c230faa4174617dfe73f538ed6603c26d123e669
Zachary Roy <zachary.roy@water.ca.gov>
2024-03-27 15:53:36 -0700

    Add batch files to run the server

---
+++ run_app_for_tests.bat
@@ -0,0 +1,10 @@
+SETLOCAL
+    CALL conda activate dev_calsim_scenario_server
+    SET database-name=test.sqlite
+    SET database-dir=.\tests\db
+    SET log-level=1
+    CALL uvicorn src.calsim_scenario_server.main:app --reload
+ENDLOCAL
+
+RMDIR ".\tests\db" /S /Q
+PAUSE
