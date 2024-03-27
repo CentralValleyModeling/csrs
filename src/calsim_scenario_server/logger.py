9ec28fc4a9fe731032861f54e0e9dd3c98fb4194
Zachary Roy <zachary.roy@water.ca.gov>
2024-03-27 13:57:24 -0700

    Encapsulate logger creation

--- src/calsim_scenario_server/logger.py
+++ src/calsim_scenario_server/logger.py
@@ -1,15 +1,33 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = TimedRotatingFileHandler("debug.log", when="d", backupCount=7)
-formatter = logging.Formatter(
-    "%(asctime)s "
-    + "[%(levelname)s] "
-    + "%(funcName)s: "
-    + "%(name)s: "
-    + "%(message)s"
-)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
+def get_dir() -> Path:
+    here = Path(".").resolve()
+    if here.name == "src":
+        here = here.parent
+
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = TimedRotatingFileHandler(
+        get_dir() / "debug.log",
+        when="d",
+        backupCount=8,  # One more day than a week
+    )
+    formatter = logging.Formatter(
+        "%(asctime)s "
+        + "[%(levelname)s] "
+        + "%(funcName)s: "
+        + "%(name)s: "
+        + "%(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+
+logger = get_logger()
