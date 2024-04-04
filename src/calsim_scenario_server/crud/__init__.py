d1764bcd0bf1f845c9191619e9c3b1519da08ab3
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-04 11:34:05 -0700

    Add submodule imports and enumeration

---
+++ src/calsim_scenario_server/crud/__init__.py
@@ -0,0 +1,13 @@
+from types import ModuleType
+from typing import Any
+
+from . import (
+    assumptions,
+    metric_values,
+    metrics,
+    paths,
+    runs,
+    scenarios,
+    timeseries_values,
+    timesteps,
+)
