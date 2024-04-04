ae05a1fef850c9bab4e0c46a7ed20ec81c9ab348
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-04 16:30:14 -0700

    Implement crud operations for Runs

--- src/calsim_scenario_server/crud/runs.py
+++ src/calsim_scenario_server/crud/runs.py
@@ -1,9 +1,67 @@
-def create():
-    raise NotImplementedError()
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from ..models import Run, RunMetadata
+from ..schemas import RunOut
 
 
-def read():
-    raise NotImplementedError()
+def create(
+    db: Session,
+    name: str,
+    scenario_id: int,
+    version: str,
+    contact: str,
+    code_version: str,
+    detail: str,
+    published: bool = False,
+    confidential: bool = True,
+    predecessor_run_name: str = None,
+) -> tuple[Run, RunMetadata]:
+    if predecessor_run_name:
+        predecessor_run = read(db, name=predecessor_run_name)
+        if len(predecessor_run) != 1:
+            raise HTTPException(
+                status_code=400, detail="multiple potential predecessors found"
+            )
+        predecessor_run_id = predecessor_run[0].id
+    else:
+        predecessor_run_id = None
+
+    # DB interactions
+    run = Run(name=name, scenario_id=scenario_id, version=version)
+    db.add(run)
+    run_metadata = RunMetadata(
+        run_id=run.id,
+        predecessor_run_id=predecessor_run_id,
+        contact=contact,
+        confidential=confidential,
+        published=published,
+        code_version=code_version,
+        detail=detail,
+    )
+    db.add(run_metadata)
+    db.commit()
+    db.refresh(run)
+    db.refresh(run_metadata)
+    return run, run_metadata
+
+
+def read(
+    db: Session,
+    name: str = None,
+    id: int = None,
+) -> list[RunOut]:
+    filters = list()
+    if name:
+        filters.append(Run.name == name)
+    if id:
+        filters.append(Run.id == id)
+    return (
+        db.query(Run)
+        .filter(*filters)
+        .join(RunMetadata, Run.id == RunMetadata.run_id)
+        .all()
+    )
 
 
 def update():
--- src/calsim_scenario_server/routes/runs.py
+++ src/calsim_scenario_server/routes/runs.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from .. import crud
 from ..database import get_db
 from ..logger import logger
-from ..models import Run, RunMetadata, Scenario
-from ..schemas import RunIn, RunOut
+from ..models import Scenario
+from ..schemas import RunIn, RunOut, RunReference
 
 router = APIRouter(prefix="/runs", tags=["Model Runs"])
 
@@ -20,73 +21,21 @@ def assert_scenario_exists(s_id: int | None, db: Session):
             )
 
 
-@router.get("/", response_model=list[RunOut])
+@router.get("", response_model=list[RunReference])
 async def get_all_runs(db: Session = Depends(get_db)):
     logger.info("getting all runs")
-    runs_table = db.query(Run).all()
-    if runs_table is None:
-        raise HTTPException(status_code=404, detail="Runs not found")
-    # FIXME: this will fail
-    runs = [RunOut(id=r.id, name=r.name, version=r.version) for r in runs_table]
+    runs = crud.runs.read(db)
     logger.info(f"{runs=}")
 
     return runs
 
 
-@router.get("/{run_id}", response_model=RunOut)
-async def get_run(run_id: int, db: Session = Depends(get_db)):
-    # FIXME, this query will probably not work without changing column names
-    run = (
-        db.query(Run)
-        .filter(Run.id == run_id)
-        .join(RunMetadata, Run.id == RunMetadata.run_id)
-        .join(Scenario, Run.scenario_id == Scenario.id)
-        .join(Run, RunMetadata.predecessor_run_id == Run.id)
-        .first()
-    )
-    if run is None:
-        raise HTTPException(status_code=404, detail="Runs not found")
-    logger.info(f"{run=}")
-    # FIXME: this will fail
-    return RunOut(
-        name=run.name,
-        version=run.version,
-        scenario=run.scenario,
-    )
-
-
-@router.put("/", response_model=RunOut)
+@router.put("", response_model=RunOut)
 async def put_run(run_data: RunIn, db: Session = Depends(get_db)):
     logger.info(run_data)
-    try:
-        assert_scenario_exists(run_data.scenario_id, db)
-        assert_scenario_exists(run_data.predecessor_run_id, db)
-        metadata = {
-            "contact",
-            "confidential",
-            "published",
-            "code_version",
-            "detail",
-            "predecessor_run_id",
-        }
-        # Create a new Path object
-        new_run = Run(**run_data.model_dump(exclude=metadata))
-        new_run_metadata = RunMetadata(**run_data.model_dump(include=metadata))
+    r, rm = crud.runs.create(db, **run_data.model_dump())
+    kwargs = run_data.model_dump()
+    kwargs["id"] = r.id
+    kwargs["predecessor_run_id"] = rm.predecessor_run_id
 
-        # Add the new path to the database session
-        db.add(new_run)
-        db.add(new_run_metadata)
-        db.commit()
-        db.refresh(new_run)
-        db.refresh(new_run_metadata)
-
-    except HTTPException as e:
-        logger.error(f"{type(e)} encountered: {e}")
-        db.rollback()
-        raise e
-    except Exception as e:
-        logger.error(f"{type(e)} encountered: {e}")
-        db.rollback()
-        raise HTTPException(status_code=500, detail=str(e))
-    run_data.id = new_run.id
-    return run_data
+    return RunOut(**kwargs)
