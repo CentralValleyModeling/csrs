3be62db4b3a9c3a71e9a33fa6d489f083d8fb7ec
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-04 09:56:58 -0700

    Add test for assumptions

--- tests/test_assumptions.py
+++ tests/test_assumptions.py
@@ -1,27 +1,43 @@
-import pytest
 import requests
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from calsim_scenario_server.database import get_db
+from calsim_scenario_server.main import app
+from calsim_scenario_server.models import Base
+
+SQLALCHEMY_DATABASE_URL = "sqlite://"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_json(url: str):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
+Base.metadata.create_all(bind=engine)
 
 
-def put_json(url: str, json):
-    response = requests.put(url, json=json)
-    response.raise_for_status()
-    return response.json()
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
-@pytest.fixture
-def base_url() -> str:
-    return "http://127.0.0.1:8000"
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
 
 
 def test_add_assumption(base_url: str):
     assumption = {"name": "example", "detail": "this is an example assumption"}
-    response = put_json(base_url + "/assumptions/tucp", assumption)
+    response = requests.put(base_url + "/assumptions/tucp", json=assumption)
+    response.raise_for_status()
     assert response["id"] == 1
 
 
