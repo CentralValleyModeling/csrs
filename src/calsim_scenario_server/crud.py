90053acc1a858e7f73d15d8f1471a113ff149a2b
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-04 10:02:32 -0700

    Split assumption crud functions

--- src/calsim_scenario_server/crud.py
+++ src/calsim_scenario_server/crud.py
@@ -1,17 +1,123 @@
+from sqlalchemy.orm import Session
+
+
 # Assumptions
-def create_assumption_dcp():
+# Land Use
+def create_assumption_land_use():
     raise NotImplementedError()
 
 
-def read_assumption():
+def read_assumption_land_use():
     raise NotImplementedError()
 
 
-def update_assumption():
+def update_assumption_land_use():
     raise NotImplementedError()
 
 
-def delete_assumption():
+def delete_assumption_land_use():
+    raise NotImplementedError()
+
+
+# Sea Level Rise
+def create_assumption_sea_level_rise():
+    raise NotImplementedError()
+
+
+def read_assumption_sea_level_rise():
+    raise NotImplementedError()
+
+
+def update_assumption_sea_level_rise():
+    raise NotImplementedError()
+
+
+def delete_assumption_sea_level_rise():
+    raise NotImplementedError()
+
+
+# Hydrology
+def create_assumption_hydrology():
+    raise NotImplementedError()
+
+
+def read_assumption_hydrology():
+    raise NotImplementedError()
+
+
+def update_assumption_hydrology():
+    raise NotImplementedError()
+
+
+def delete_assumption_hydrology():
+    raise NotImplementedError()
+
+
+# TUCP
+def create_assumption_tucp():
+    raise NotImplementedError()
+
+
+def read_assumption_tucp():
+    raise NotImplementedError()
+
+
+def update_assumption_tucp():
+    raise NotImplementedError()
+
+
+def delete_assumption_tucp():
+    raise NotImplementedError()
+
+
+# Delta Conveyance Project
+def create_assumption_delta_conveyance_project():
+    raise NotImplementedError()
+
+
+def read_assumption_delta_conveyance_project():
+    raise NotImplementedError()
+
+
+def update_assumption_delta_conveyance_project():
+    raise NotImplementedError()
+
+
+def delete_assumption_delta_conveyance_project():
+    raise NotImplementedError()
+
+
+# Voluntary Agreements
+def create_assumption_voluntary_agreements():
+    raise NotImplementedError()
+
+
+def read_assumption_voluntary_agreements():
+    raise NotImplementedError()
+
+
+def update_assumption_voluntary_agreements():
+    raise NotImplementedError()
+
+
+def delete_assumption_voluntary_agreements():
+    raise NotImplementedError()
+
+
+# South of Delta Storage
+def create_assumption_south_of_delta_storage():
+    raise NotImplementedError()
+
+
+def read_assumption_south_of_delta_storage():
+    raise NotImplementedError()
+
+
+def update_assumption_south_of_delta_storage():
+    raise NotImplementedError()
+
+
+def delete_assumption_south_of_delta_storage():
     raise NotImplementedError()
 
 
