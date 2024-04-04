e6d15673600d7ebc44d20fcb8490687905de405f
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-04 09:56:03 -0700

    Add empty `crud` module

---
+++ src/calsim_scenario_server/crud.py
@@ -0,0 +1,134 @@
+# Assumptions
+def create_assumption_dcp():
+    raise NotImplementedError()
+
+
+def read_assumption():
+    raise NotImplementedError()
+
+
+def update_assumption():
+    raise NotImplementedError()
+
+
+def delete_assumption():
+    raise NotImplementedError()
+
+
+# Paths
+def create_path():
+    raise NotImplementedError()
+
+
+def read_path():
+    raise NotImplementedError()
+
+
+def update_path():
+    raise NotImplementedError()
+
+
+def delete_path():
+    raise NotImplementedError()
+
+
+# Timesteps
+def create_timestep():
+    raise NotImplementedError()
+
+
+def read_timestep():
+    raise NotImplementedError()
+
+
+def update_timestep():
+    raise NotImplementedError()
+
+
+def delete_timestep():
+    raise NotImplementedError()
+
+
+# Metrics
+def create_metric():
+    raise NotImplementedError()
+
+
+def read_metric():
+    raise NotImplementedError()
+
+
+def update_metric():
+    raise NotImplementedError()
+
+
+def delete_metric():
+    raise NotImplementedError()
+
+
+# Scenarios
+def create_scenario():
+    raise NotImplementedError()
+
+
+def read_scenario():
+    raise NotImplementedError()
+
+
+def update_scenario():
+    raise NotImplementedError()
+
+
+def delete_scenario():
+    raise NotImplementedError()
+
+
+# Runs
+def create_run():
+    raise NotImplementedError()
+
+
+def read_run():
+    raise NotImplementedError()
+
+
+def update_run():
+    raise NotImplementedError()
+
+
+def delete_run():
+    raise NotImplementedError()
+
+
+# TimeSeriesValues
+def create_timeseries_value():
+    raise NotImplementedError()
+
+
+def read_timeseries_value():
+    raise NotImplementedError()
+
+
+def update_timeseries_value():
+    raise NotImplementedError()
+
+
+def delete_timeseries_value():
+    raise NotImplementedError()
+
+
+# Metric Values
+def create_metric_value():
+    raise NotImplementedError()
+
+
+def read_metric_value():
+    raise NotImplementedError()
+
+
+def update_metric_value():
+    raise NotImplementedError()
+
+
+def delete_metric_value():
+    raise NotImplementedError()
