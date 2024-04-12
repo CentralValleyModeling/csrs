ff90b1f7de0304199ac0a22e542bda3097b32b20
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-12 16:06:27 -0700

    Update tests for new schema, and client class

--- tests/test_assumptions.py
+++ tests/test_assumptions.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from fastapi.testclient import TestClient
@@ -7,11 +8,13 @@ from sqlalchemy.pool import StaticPool
 
 sys.path.append("./src")
 
+from calsim_scenario_server.client import CalSimScenarioClient
 from calsim_scenario_server.database import get_db
 from calsim_scenario_server.main import app
 from calsim_scenario_server.models import Base
+from calsim_scenario_server.schemas import Assumption, Scenario
 
-SQLALCHEMY_DATABASE_URL = "sqlite://"
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
@@ -32,55 +35,64 @@ def get_testing_db():
 
 
 app.dependency_overrides[get_db] = get_testing_db
-client = TestClient(app)
+test_client = TestClient(app)
 
 
-def test_add_assumption():
-    assumption = {"name": "example", "detail": "this is an example assumption"}
-    response = client.put("/assumptions/tucp", json=assumption)
+def test_testing_on_local():
+    from pathlib import Path
+
+    import calsim_scenario_server
+
+    here = Path(__file__).parent
+    mod_path = Path(calsim_scenario_server.__file__).parent
+
+    rel = mod_path.relative_to(here.parent)
+    assert str(rel) == "src\\calsim_scenario_server"
+
+
+def test_put_assumption():
+    a_in = Assumption(
+        name="example",
+        detail="this is an example assumption",
+        kind="tucp",
+    )
+    print(a_in.model_dump_json(exclude="id"))
+    response = test_client.put("/assumptions/tucp", json=a_in.model_dump(exclude="id"))
     response.raise_for_status()
-    values = response.json()
-    assert values["id"] == 1
+    a_out = Assumption.model_validate(response.json())
+    assert a_out.id == 1
 
 
 def test_get_assumption():
-    response = client.get("/assumptions/tucp")
-    response.raise_for_status()
-    values = response.json()
+    client = CalSimScenarioClient(base_url="http://localhost")
+    client.client = test_client
+    values = client.get_assumption(kind="tucp")
     assert len(values) == 1
-    assert values[0]["id"] == 1
+    assert values[0].id == 1
 
 
 def test_put_scenario():
-    basic_info = {"name": "add_sceanrio_example", "detail": "foo bar baz"}
-    assumptions = {
-        "dcp": basic_info,
-        "hydrology": basic_info,
-        "land_use": dict(additional_metadata={"future_year": 2020}, **basic_info),
-        "sea_level_rise": dict(additional_metadata={"centimeters": 0}, **basic_info),
-        "sod": basic_info,
-        "tucp": basic_info,
-        "va": basic_info,
-    }
-    response = client.get("/assumptions")
+    assumpt_base = {"name": "add_sceanrio_example", "detail": "foo bar baz"}
+    assumptions = {c: {"kind": c, **assumpt_base} for c in Scenario.model_fields}
+    response = test_client.get("/assumptions")
     response.raise_for_status()
     values = response.json()
-    id_values = dict()
+    names = dict()
     for name in values:
-        response = client.put(f"/assumptions/{name}", json=assumptions[name])
+        response = test_client.put(f"/assumptions/{name}", json=assumptions[name])
         response.raise_for_status()
         assumption = response.json()
-        id_values[name] = {"id": assumption["id"]}
-    scenario_data = dict(name="test_add_scenario", assumptions_used=id_values)
-    print(scenario_data)
-    response = client.put("/scenarios", json=scenario_data)
+        names[name] = assumption["name"]
+    print(names)
+    scenario_data = dict(name="test_add_scenario", **names)
+    response = test_client.put("/scenarios", json=scenario_data)
     response.raise_for_status()
     value = response.json()
     assert value["name"] == "test_add_scenario"
 
 
 def test_get_scenario():
-    response = client.get("/scenarios")
+    response = test_client.get("/scenarios")
     response.raise_for_status()
     values = response.json()
     assert len(values) == 1
