114a5fba4b9df956a592c16b821a96a524165bdf
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-15 15:03:55 -0700

    Add enumerations and fix errors in models

--- calsim_scenario_server/models.py
+++ calsim_scenario_server/models.py
@@ -23,6 +23,27 @@ class AssumptionEnumeration(enum.StrEnum):
     south_of_delta = "south_of_delta"
 
 
+class DimensionalityEnumeration(enum.StrEnum):
+    volume = "[length] ** 3"
+    area = "[length] ** 2"
+    length = "[length]"
+    flow = "[length] ** 3 / [time]"
+    flux = "[length] ** 2 / [time]"
+    mass = "[mass]"
+    mass_flow = "[mass] / [time]"
+    temperature = "[temperature]"
+
+
+class PathCategoryEnumeration(enum.StrEnum):
+    delivery = "delivery"
+    delta = "delta"
+    other = "other"
+    salinity = "salinity"
+    storage = "storage"
+    upstream_flows = "upstream_flows"
+    water_year_type = "wyt"
+
+
 # Define ORM models for each table
 class RunModel(Base):
     __tablename__ = "runs"
@@ -87,8 +108,10 @@ class NamedPathModel(Base):
     id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
     name = mapped_column(String)
     path = mapped_column(String)
-    category = mapped_column(String)
-    units = mapped_column(ForeignKey("units.name"))
+    category = mapped_column(
+        sqlalchemyEnum(PathCategoryEnumeration),
+        nullable=False,
+    )
     detail = mapped_column(String, nullable=False)
 
     __table_args__ = (UniqueConstraint("path", "category", name="unique_purpose"),)
@@ -99,23 +122,27 @@ class UnitModel(Base):
 
     id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
     name = mapped_column(String, unique=True)
+    dimensionality = mapped_column(
+        sqlalchemyEnum(DimensionalityEnumeration),
+        nullable=False,
+    )
 
 
 class MetricModel(Base):
     __tablename__ = "metrics"
 
     id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = mapped_column(String, unique=True)
     index_detail = mapped_column(String, nullable=False)
-    units = mapped_column(ForeignKey("units.name"))
     detail = mapped_column(String, nullable=False)
 
 
 class TimeseriesValueModel(Base):
     __tablename__ = "time_series_values"
-    id = mapped_column(Integer, primary_key=True, autoincrement=True)
-    run_id = mapped_column(ForeignKey("runs.id"))
-    path_id = mapped_column(ForeignKey("paths.id"))
-    timestep_id = mapped_column(ForeignKey("timesteps.id"))
+    run_id = mapped_column(ForeignKey("runs.id"), primary_key=True)
+    path_id = mapped_column(ForeignKey("paths.id"), primary_key=True)
+    timestep_id = mapped_column(ForeignKey("timesteps.id"), primary_key=True)
+    units = mapped_column(ForeignKey("units.id"))
     value = mapped_column(Float, nullable=False)
 
     __table_args__ = (
@@ -129,6 +156,7 @@ class MetricValueModel(Base):
     run_id = mapped_column(ForeignKey("runs.id"), primary_key=True)
     metric_id = mapped_column(ForeignKey("metrics.id"), primary_key=True)
     index = mapped_column(Integer, nullable=False)
+    units = mapped_column(ForeignKey("units.name"))
     value = mapped_column(Float, nullable=False)
 
 
