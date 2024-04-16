9a314fd164f5610b8613039888af01efb6bae59b
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-16 13:49:10 -0700

    Add docstrings to models

--- calsim_scenario_server/models.py
+++ calsim_scenario_server/models.py
@@ -15,8 +15,68 @@ class Base(DeclarativeBase):
     pass
 
 
+class AssumptionModel(Base):
+    """Data regarding single modeling assumptions."""
+
+    __tablename__ = "assumptions"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    kind: Mapped[AssumptionEnum] = mapped_column(nullable=False)
+    detail: Mapped[str] = mapped_column()
+    # ORM relationships
+    scenario_map: Mapped[list["ScenarioAssumptionsModel"]] = relationship(
+        back_populates="assumption"
+    )
+    # Multi-column unique rules
+    __table_args__ = (
+        UniqueConstraint(
+            "name",
+            "kind",
+            name="unique_name",
+        ),
+        UniqueConstraint(
+            "detail",
+            "kind",
+            name="unique_detail",
+        ),
+    )
+
+
+class ScenarioAssumptionsModel(Base):
+    """Data regarding which modeling scenarios use which assumptions."""
+
+    __tablename__ = "scenario_assumptions"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    scenario_id: Mapped[int] = mapped_column(ForeignKey("scenarios.id"), nullable=False)
+    assumption_kind: Mapped[AssumptionEnum] = mapped_column(nullable=False)
+    assumption_id: Mapped[int] = mapped_column(
+        ForeignKey("assumptions.id"), nullable=False
+    )
+    # ORM relationships
+    scenario: Mapped["ScenarioModel"] = relationship(back_populates="assumption_maps")
+    assumption: Mapped["AssumptionModel"] = relationship(back_populates="scenario_map")
+
+
+class ScenarioModel(Base):
+    """Data establishing modeling scenarios."""
+
+    __tablename__ = "scenarios"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    # ORM relationships
+    runs: Mapped[list["RunModel"]] = relationship(back_populates="scenario")
+    assumption_maps: Mapped[list["ScenarioAssumptionsModel"]] = relationship(
+        back_populates="scenario"
+    )
+
+
 # Define ORM models for each table
 class RunModel(Base):
+    """Data and metadata establishing model runs."""
+
     __tablename__ = "runs"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
@@ -46,33 +106,9 @@ class RunModel(Base):
     )
 
 
-class ScenarioModel(Base):
-    __tablename__ = "scenarios"
-
-    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(nullable=False, unique=True)
-    # ORM relationships
-    runs: Mapped[list[RunModel]] = relationship(back_populates="scenario")
-    assumption_maps: Mapped[list["ScenarioAssumptionsModel"]] = relationship(
-        back_populates="scenario"
-    )
-
-
-class ScenarioAssumptionsModel(Base):
-    __tablename__ = "scenario_assumptions"
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    scenario_id: Mapped[int] = mapped_column(ForeignKey("scenarios.id"), nullable=False)
-    assumption_kind: Mapped[AssumptionEnum] = mapped_column(nullable=False)
-    assumption_id: Mapped[int] = mapped_column(
-        ForeignKey("assumptions.id"), nullable=False
-    )
-    # ORM relationships
-    scenario: Mapped[ScenarioModel] = relationship(back_populates="assumption_maps")
-    assumption: Mapped["AssumptionModel"] = relationship(back_populates="scenario_map")
-
-
 class NamedPathModel(Base):
+    """Data about the meaning and A-F representation of DSS Paths."""
+
     __tablename__ = "paths"
 
     id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
@@ -85,6 +121,8 @@ class NamedPathModel(Base):
 
 
 class UnitModel(Base):
+    """Data about the units used by timeseries and metrics."""
+
     __tablename__ = "units"
 
     id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
@@ -93,6 +131,8 @@ class UnitModel(Base):
 
 
 class MetricModel(Base):
+    """Data about the meaning of metrics."""
+
     __tablename__ = "metrics"
 
     id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
@@ -101,26 +141,9 @@ class MetricModel(Base):
     detail = mapped_column(String, nullable=False)
 
 
-class TimeSeriesModel(Base):
-    __tablename__ = "time_series_values"
-
-    run_id = mapped_column(ForeignKey("runs.id"), primary_key=True)
-    path_id = mapped_column(ForeignKey("paths.id"), primary_key=True)
-    timestep_id = mapped_column(ForeignKey("timesteps.id"), primary_key=True)
-    units = mapped_column(ForeignKey("units.id"))
-    value = mapped_column(Float, nullable=False)
-    # Multi-column unique rules
-    __table_args__ = (
-        UniqueConstraint(
-            "run_id",
-            "path_id",
-            "timestep_id",
-            name="unique_in_run",
-        ),
-    )
-
-
 class MetricValueModel(Base):
+    """Data for the processed metrics."""
+
     __tablename__ = "metric_values"
 
     path_id = mapped_column(ForeignKey("paths.id"), primary_key=True)
@@ -129,36 +152,3 @@ class MetricValueModel(Base):
     index = mapped_column(Integer, nullable=False)
     units = mapped_column(ForeignKey("units.name"))
     value = mapped_column(Float, nullable=False)
-
-
-class TimestepModel(Base):
-    __tablename__ = "timesteps"
-
-    id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
-    datetime_str = mapped_column(String, nullable=False, unique=True)
-
-
-class AssumptionModel(Base):
-    __tablename__ = "assumptions"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    kind: Mapped[AssumptionEnum] = mapped_column(nullable=False)
-    detail: Mapped[str] = mapped_column()
-    # ORM relationships
-    scenario_map: Mapped[list[ScenarioAssumptionsModel]] = relationship(
-        back_populates="assumption"
-    )
-    # Multi-column unique rules
-    __table_args__ = (
-        UniqueConstraint(
-            "name",
-            "kind",
-            name="unique_name",
-        ),
-        UniqueConstraint(
-            "detail",
-            "kind",
-            name="unique_detail",
-        ),
-    )
