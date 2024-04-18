5ba093a89ef49ce46666f17b96c7089cee0383b4
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-18 10:52:50 -0700

    Reorganize client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,90 +1,104 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
+from calsim_scenario_server import clients, schemas
 
-from calsim_scenario_server import CalSimScenarioClient, app
-from calsim_scenario_server.database import get_db
-from calsim_scenario_server.logger import logger
-from calsim_scenario_server.models import Base
+TESTING_DATA = {
+    "assumptions": [
+        {
+            "name": "testing-client-assumption-land-use",
+            "kind": "land_use",
+            "detail": "test assumption, land use",
+        },
+        {
+            "name": "testing-client-assumption-sea-level-rise",
+            "kind": "sea_level_rise",
+            "detail": "test assumption, sea level rise",
+        },
+        # Two hydrology assumptions
+        {
+            "name": "testing-client-assumption-hydrology-1",
+            "kind": "hydrology",
+            "detail": "test assumption, hydrology 1",
+        },
+        {
+            "name": "testing-client-assumption-hydrology-2",
+            "kind": "hydrology",
+            "detail": "test assumption, hydrology 2",
+        },
+        {
+            "name": "testing-client-assumption-tucp",
+            "kind": "tucp",
+            "detail": "test assumption, tucp",
+        },
+        {
+            "name": "testing-client-assumption-dcp",
+            "kind": "dcp",
+            "detail": "test assumption, dcp",
+        },
+        {
+            "name": "testing-client-assumption-va",
+            "kind": "va",
+            "detail": "test assumption, va",
+        },
+        {
+            "name": "testing-client-assumption-south-of-delta",
+            "kind": "south_of_delta",
+            "detail": "test assumption, south of delta",
+        },
+    ],
+    "scearios": [
+        {
+            "name": "testing-client-scenario-1",
+            "version": None,
+            "land_use": "testing-client-assumption-land-use",
+            "sea_level_rise": "testing-client-assumption-sea-level-rise",
+            "hydrology": "testing-client-assumption-hydrology-1",
+            "tucp": "testing-client-assumption-tucp",
+            "dcp": "testing-client-assumption-dcp",
+            "va": "testing-client-assumption-va",
+            "south_of_delta": "testing-client-assumption-south-of-delta",
+        },
+        {
+            "name": "testing-client-scenario-2",
+            "version": None,
+            "land_use": "testing-client-assumption-land-use",
+            "sea_level_rise": "testing-client-assumption-sea-level-rise",
+            "hydrology": "testing-client-assumption-hydrology-2",
+            "tucp": "testing-client-assumption-tucp",
+            "dcp": "testing-client-assumption-dcp",
+            "va": "testing-client-assumption-va",
+            "south_of_delta": "testing-client-assumption-south-of-delta",
+        },
+    ],
+    "runs": [
+        {
+            "scenario": "testing-client-scenario-1",
+            "version": "0.1",
+            "contact": "testing@email.com",
+            "code_version": "9.0.0",
+            "detail": "Dummy run to test the API client. Scenario 1, is version 0.1",
+        },
+        {
+            "scenario": "testing-client-scenario-1",
+            "version": "0.2",
+            "parent": "0.1",
+            "contact": "update@email.com",
+            "code_version": "9.0.1",
+            "detail": """Dummy run to test the API client. Scenario 1, is version 0.2,
+                        updates were made to the model code.""",
+        },
+        {
+            "scenario": "testing-client-scenario-2",
+            "version": "0.1",
+            "contact": "other@email.com",
+            "code_version": "10.0.0",
+            "confidential": False,
+            "published": True,
+            "detail": "Dummy run to test the API client. Scenario 2, is version 0.1",
+        },
+    ],
+}
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
-    echo=True,
-)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
-
-
-def get_testing_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = get_testing_db
-client = CalSimScenarioClient(base_url="http://localhost")
-client.actor = TestClient(app)  # TODO: move this to a method
-
-
-def test_testing_on_local():
-    logger.debug("begin unit test")
-    from pathlib import Path
-
-    import calsim_scenario_server
-
-    here = Path(__file__).parent
-    mod_path = Path(calsim_scenario_server.__file__).parent
-
-    rel = mod_path.relative_to(here.parent)
-    assert str(rel) == "calsim_scenario_server"
-
-
-def test_put_assumption():
-    logger.info("begin unit test")
-    assumption = client.put_assumption(
-        name="example",
-        kind="tucp",
-        detail="foo bar baz",
-    )
-    assert assumption.id == 1
-
-
-def test_get_assumption():
-    logger.info("begin unit test")
-    values = client.get_assumption(kind="tucp")
-    assert len(values) == 1
-    assert values[0].id == 1
-
-
-def test_put_scenario():
-    logger.info("begin unit test")
-    assumpt_base = {"name": "add_scenario_example", "detail": "foo bar baz2"}
-    assumption_names = client.get_assumption_names()
-    scenario_kwargs = dict()
-    for name in assumption_names:
-        added = client.put_assumption(kind=name, **assumpt_base)
-        scenario_kwargs[name] = added.name
-
-    added = client.put_scenario(name="test_add_scenario", **scenario_kwargs)
-    assert added.name == "test_add_scenario"
-
-
-def test_get_scenario():
-    logger.info("begin unit test")
-    scenarios = client.get_scenario(name="test_add_scenario")
-    assert len(scenarios) == 1
-    scenario = scenarios[0]
-    assert scenario.name == "test_add_scenario"
-    assert scenario.dcp == "add_scenario_example"
-
-
-# TODO: write tests for client adding runs
-# TODO: write tests for client adding paths
-# TODO: write tests for client addning timeseries
+def test_local_assumptions(client_local: clients.LocalClient):
+    for assumption in TESTING_DATA["assumptions"]:
+        s = client_local.put_assumption(**assumption)
+        assert isinstance(s, schemas.Assumption)
