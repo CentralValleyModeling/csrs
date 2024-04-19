6a3885b2dfd1d1bf67d77924b32dd068f348dbb5
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-19 16:01:38 -0700

    Lookup package version at runtime from version in build metadata

--- calsim_scenario_server/__init__.py
+++ calsim_scenario_server/__init__.py
@@ -1,4 +1,11 @@
-__version__ = "0.0.2"
+from importlib.metadata import PackageNotFoundError, version
 
 from .clients import LocalClient, RemoteClient
 from .main import app
+from .schemas import Assumption, NamedDatasetPath, Run, Scenario, Timeseries
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # pandss not installed, likely developer mode
+    __version__ = None
