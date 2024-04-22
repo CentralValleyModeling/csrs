1ebf1c6c98aebb91d5d66cea698320dd63e4eb4a
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-22 11:30:05 -0700

    Add route for paths

---
+++ calsim_scenario_server/routes/paths.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from .. import crud, schemas
+from ..database import get_db
+from ..logger import logger
+
+router = APIRouter(prefix="/paths", tags=["Paths"])
+
+
+@router.get("", response_model=list[schemas.NamedDatasetPath])
+async def get_paths(
+    name: str = None,
+    path: str = None,
+    category: str = None,
+    db: Session = Depends(get_db),
+):
+    logger.info(f"getting all runs, filters, {name=}, {path=}, {category=}")
+    paths = crud.paths.read(
+        db=db,
+        name=name,
+        path=path,
+        category=category,
+    )
+    logger.info(f"paths: {[(p.name, p.path) for p in paths]}")
+
+    return paths
+
+
+@router.put("", response_model=schemas.NamedDatasetPath)
+async def put_path(
+    _in: schemas.NamedDatasetPath,
+    db: Session = Depends(get_db),
+):
+    logger.info(_in)
+    _out = crud.paths.create(
+        db=db,
+        **_in.model_dump(exclude=("id")),
+    )
+    logger.debug(f"new path {_out.id=}")
+    return _out
