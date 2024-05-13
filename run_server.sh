01b1b4cda3506ddebee3ae109bbe28aba048a12e
Zachary Roy <zachary.roy@water.ca.gov>
2024-05-13 14:14:01 -0700

    Init conda for bash

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,5 @@
 # assume that the container already has activated the right environment
 # asumme current working directory is at the top level of this repo
+conda init bash
 conda activate csrs
 uvicorn src.csrs.main:app --host 0.0.0.0 --port 80
