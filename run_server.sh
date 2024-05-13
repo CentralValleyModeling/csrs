b2a67b49ad841c6ab856a117f0724d214df525e7
Zachary Roy <zachary.roy@water.ca.gov>
2024-05-13 14:10:09 -0700

    Activate conda normally

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,4 @@
 # assume that the container already has activated the right environment
 # asumme current working directory is at the top level of this repo
-. /opt/conda/etc/profile.d/conda.sh
 conda activate csrs
 uvicorn src.csrs.main:app --host 0.0.0.0 --port 80
