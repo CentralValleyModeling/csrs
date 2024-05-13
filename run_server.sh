0ce4c96df6fa6f67ffce0ced63233403bdbbe9f2
Zachary Roy <zachary.roy@water.ca.gov>
2024-05-13 15:37:49 -0700

    Add comments, copy databases

--- run_server.sh
+++ run_server.sh
@@ -1,14 +1,29 @@
-# assume that the container already has activated the right environment
-# asumme current working directory is at the top level of this repo
-# Build should pass the GITHUB_TOKEN as a --build_arg
-git_repo_url="https://$GITHUB_TOKEN@github.com/CentralValleyModeling/csrs"
+#!/bin/bash
+
+# Activate environment
+source /env/bin/activate
+
+# Check if the GITHUB_TOKEN environment variable is set 
+# (should be passed to docker build as a build-arg)
 if [ -z "$GITHUB_TOKEN" ]; then
-    echo "Warning: GITHUB_TOKEN environment variable is not set, assuming public repo"
-    git_repo_url="https://github.com/CentralValleyModeling/csrs"
+    echo "assuming repo is public, no token set"
+    git_repo_url="https://github.com/$1.git"
+else
+    echo "github token set"
+    git_repo_url="https://$GITHUB_TOKEN@github.com/$1.git"
 fi
 
-echo "git_repo = ${git_repo_url}"
-git clone "${git_repo_url}" "code"
+# Clone the repo into a subfolder
+echo "cloning repo $1"
+git clone "$git_repo_url" "code"
+
+# Copy over any bootstrap databases
+cp -R database/. code/database/
+
+# Change directory to the cloned repository
+cd "code" || exit
 
 # Run app
-uvicorn code.src.csrs.main:app --host 0.0.0.0 --port 80
+echo "Running the application..."
+
+uvicorn src.csrs.main:app --host 0.0.0.0 --port 80
