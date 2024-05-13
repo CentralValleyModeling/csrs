637087ab8c9a7e3d8e77d2a5b87cfa548f536320
Zachary Roy <zachary.roy@water.ca.gov>
2024-05-13 14:09:05 -0700

    Update comment

--- src/csrs/__init__.py
+++ src/csrs/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:
-    # pandss not installed, likely developer mode
+    # csrs not installed, likely developer mode
     __version__ = None
 from .clients import LocalClient, RemoteClient
 from .schemas import Assumption, NamedPath, Run, Scenario, Timeseries
