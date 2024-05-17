a334da2e8d5063504442456d69707f482527889d
Zachary Roy <zachary.roy@water.ca.gov>
2024-05-17 09:16:01 -0700

    Clone from production branch

--- run_server.sh
+++ run_server.sh
@@ -3,19 +3,11 @@
 # Activate environment
 source /env/bin/activate
 
-# Check if the GITHUB_TOKEN environment variable is set 
-# (should be passed to docker build as a build-arg)
-if [ -z "$GITHUB_TOKEN" ]; then
-    echo "assuming repo is public, no token set"
-    git_repo_url="https://github.com/$1.git"
-else
-    echo "github token set"
-    git_repo_url="https://$GITHUB_TOKEN@github.com/$1.git"
-fi
+git_repo_url="https://github.com/$1.git"
 
 # Clone the repo into a subfolder
 echo "cloning repo $1"
-git clone "$git_repo_url" "code"
+git clone -b production "$git_repo_url" "code"
 
 # Copy over any bootstrap databases
 cp -R database/. code/database/
