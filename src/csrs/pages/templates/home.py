cec56d0e56c31f9de0de363efbe23632cf7c03c1
Zachary Roy <zachary.roy@water.ca.gov>
2024-07-23 14:10:43 -0700

    Hide links to edit pages when edit is off

--- src/csrs/pages/templates/home.py
+++ src/csrs/pages/templates/home.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from fastapi import Request
 from jinja2 import Environment
 
+from ..edit.render import ALLOW_EDITING_VIA_FORMS
 from ..loader import ENV
 
 
@@ -13,7 +14,10 @@ class Home:
     template: str = "templates/home.jinja"
 
     def __str__(self) -> str:
-        return self.env.get_template(self.template).render(request=self.request)
+        return self.env.get_template(self.template).render(
+            request=self.request,
+            edit_on=ALLOW_EDITING_VIA_FORMS,
+        )
 
     def encode(self, charset: str) -> str:
         return str(self).encode(encoding=charset)
